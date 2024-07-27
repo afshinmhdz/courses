de0dd6ed8b0ed8a0a2a2471fcffd04455b4fd34b
afshinmhdz <afshinmhdz1906@gmail.com>
2024-07-27 19:36:55 +0330

    extract two component inside courseCard component with name  CourseCardBody CourseCardFooter

--- src/Component/CourseCard.jsx
+++ src/Component/CourseCard.jsx
@@ -1,44 +1,51 @@
-import React from 'react'
+import React from "react";
 
-function CourseCard({course}) {
-    
-        
-        
-        return (
-          <div key={course.id} className="course-item">
-            <div className="course-item__img">
-              <img src={course.imageUrl} alt="" />
-            </div>
-            <div className="course-item__detail">
-              <div className="course-item__body">
-                <div>
-                  <p className="title">{course.title}</p>
-                  <p className="desc">{course.description}</p>
-                </div>
-                <span className="rate">{course.rate}</span>
-              </div>
-              <div className="course-item__footer">
-                <div className="tags">
-
-                    {course.tags.map((t)=>{
-                        return(<span className="badge badge--secondary">{t}</span>)
-                    })}
-                </div>
-                <div className="caption">
-                  <div className="date">
-                    {new Date(course.start).toLocaleDateString("fa-IR", {
-                      day: "numeric",
-                      month: "short",
-                      year: "numeric",
-                    })}
-                  </div>
-                  <span className="badge badge--primary">{course.status}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      
+function CourseCard({ course }) {
+  return (
+    <div  className="course-item">
+      <div className="course-item__img">
+        <img src={course.imageUrl} alt="" />
+      </div>
+      <div className="course-item__detail">
+        <CourseCardBody title={course.title} description={course.description} rate={course.rate}/>
+        <CourseCardFooter tags={course.tags} start={course.start} status={course.status}/>
+      </div>
+    </div>
+  );
 }
 
-export default CourseCard
+export default CourseCard;
+
+function CourseCardBody({title,description,rate}) {
+  return (
+    <div className="course-item__body">
+      <div>
+        <p className="title">{title}</p>
+        <p className="desc">{description}</p>
+      </div>
+      <span className="rate">{rate}</span>
+    </div>
+  );
+}
+
+function CourseCardFooter({tags,start,status}) {
+  return (
+    <div className="course-item__footer">
+      <div className="tags">
+        {tags.map((t) => {
+          return <span key={t} className="badge badge--secondary">{t}</span>;
+        })}
+      </div>
+      <div className="caption">
+        <div className="date">
+          {new Date(start).toLocaleDateString("fa-IR", {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+          })}
+        </div>
+        <span className={`badge ${status==="Active" ? "badge--primary" : status==="Upcoming" ?  "badge--secondary": "badge--danger" }`}>{status}</span>
+      </div>
+    </div>
+  );
+}
